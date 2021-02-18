6ce463b5e4c29c23d8f85136e569c8d2094a0950
spfox24 <68411909+spfox24@users.noreply.github.com>
2021-02-17 22:01:03 -0600

    adds M2M rel for cases and guitars with remove

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 SESSIONS = (
     ('THEORY', 'Theory'),
@@ -16,6 +17,16 @@ TIME = (
     ('60', '60 minutes'),
 )
 
+class Case(models.Model):
+    case = models.CharField(max_length=50)
+    brand = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.brand
+
+    def get_absolute_url(self):
+        return reverse('cases_detail', kwargs={'case_id': self.id})
+
 class Guitar(models.Model):
     brand = models.CharField(max_length=250)
     model = models.CharField(max_length=250)
@@ -23,18 +34,23 @@ class Guitar(models.Model):
     year = models.IntegerField()
     description = models.TextField(max_length=250)
 
+    cases = models.ManyToManyField(Case)
+
     def __str__(self):
         return self.model
     
     def get_absolute_url(self):
         return reverse('guitars_detail', kwargs={'guitar_id': self.id})
 
+    def practiced_for_today(self):
+        return self.practice_set.filter(date=date.today()).count() >= 1
+
 class Practice(models.Model):
     date = models.DateField('Practice Date')
     time = models.CharField(
         max_length=2,
         choices=TIME,
-        default=TIME[0][0]
+        default=TIME[0][0],
     )
     focus = models.CharField(
         max_length=50,
@@ -49,15 +65,5 @@ class Practice(models.Model):
 
     class Meta:
         ordering: ['-date']
-
-class Case(models.Model):
-    case = models.CharField(max_length=50)
-    brand = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.brand
-
-    def get_absolute_url(self):
-        return reverse('cases_detail', kwargs={'case_id': self.id})
 
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Guitar, Case
-
 from .forms import PracticeForm
 
 def home(request):
@@ -16,8 +15,9 @@ def guitars_index(request):
 
 def guitars_detail(request, guitar_id):
     guitar = Guitar.objects.get(id=guitar_id)
+    cases_excluded = Case.objects.exclude(id__in = guitar.cases.all().values_list('id'))
     practice_form = PracticeForm()
-    return render(request, 'guitars/detail.html', { 'guitar': guitar, 'practice_form': practice_form })
+    return render(request, 'guitars/detail.html', { 'guitar': guitar, 'practice_form': practice_form, 'cases': cases_excluded })
 
 def add_practice(request, guitar_id):
     form = PracticeForm(request.POST)
@@ -25,6 +25,14 @@ def add_practice(request, guitar_id):
         new_practice = form.save(commit=False)
         new_practice.guitar_id = guitar_id
         new_practice.save()
+    return redirect('guitars_detail', guitar_id=guitar_id)
+
+def assoc_case(request, guitar_id, case_id):
+    Guitar.objects.get(id=guitar_id).cases.add(case_id)
+    return redirect('guitars_detail', guitar_id=guitar_id)
+
+def remove_case(request, guitar_id, case_id):
+    Guitar.objects.get(id=guitar_id).cases.remove(case_id)
     return redirect('guitars_detail', guitar_id=guitar_id)
 
 class GuitarCreate(CreateView):
