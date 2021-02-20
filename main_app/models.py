21c5053a4ee79d3b0cfb7a0fb4d708c0622dbf0e
spfox24 <68411909+spfox24@users.noreply.github.com>
2021-02-20 15:22:33 -0600

    adds photo model

--- main_app/models.py
+++ main_app/models.py
@@ -66,4 +66,13 @@ class Practice(models.Model):
     class Meta:
         ordering: ['-date']
 
+class Photo(models.Model):
+    url = models.CharField(max_length=200)
+    cat = models.ForeignKey(Guitar, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Photo for guitar_id: {self.guitar_id} @{self.url}"
+
+
+
 
